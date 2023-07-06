0d1b307cde4dc4155b404107334eba8381644b1d
bozhidarkapudzhiev <bozhidar_kapudzhiev@abv.bg>
2023-07-06 19:45:22 +0300

    Programming Basic Java July 2023 example's ane tasks's

--- src/_08_07_2023_ConditionalStatements/_6_SpeedInfo.java
+++ src/_08_07_2023_ConditionalStatements/_6_SpeedInfo.java
@@ -8,13 +8,13 @@ public class _6_SpeedInfo {
         int number=Integer.parseInt(scanner.nextLine());
         if (number<=10){
             System.out.println("slow");
-        }else if((number>10)&&(number<=50)){
+        }else if(number<=50){
             System.out.println("average");
-        }else if ((number>50)&&(number<=150)){
+        }else if (number<=150){
             System.out.println("fast");
-        }else if ((number>150)&&(number<=1000)){
+        }else if (number<=1000){
             System.out.println("ultra fast");
-        }else if (number>1000){
+        }else {
             System.out.println("extremely fast");
         }
     }
--- src/_08_07_2023_ConditionalStatements/_7_AreaOfFigures_Copy.java
+++ src/_08_07_2023_ConditionalStatements/_7_AreaOfFigures_Copy.java
@@ -5,27 +5,23 @@ import java.util.Scanner;
 public class _7_AreaOfFigures_Copy {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        String geometricTypes= scanner.nextLine();
-        switch (geometricTypes){
-            case "square":
-                double a=Double.parseDouble(scanner.nextLine());
-                double areaSquare=a*a;
-                System.out.println(areaSquare);
-            case "rectangle":
-                int width=Integer.parseInt(scanner.nextLine());
-                double heigth=Double.parseDouble(scanner.nextLine());
-                double areaRectangle=heigth*width;
-                System.out.println(areaRectangle);
-            case "circle":
-                double radius=Double.parseDouble(scanner.nextLine());
-                double radiusArea=Math.PI*(radius*radius);
-                System.out.printf("%.3f",radiusArea);
-            case"triangle":
-                double triangleA=Double.parseDouble(scanner.nextLine());
-                int triangleB=Integer.parseInt(scanner.nextLine());
-                double triangleArea=(triangleA*triangleB)/2;
-                System.out.println(triangleArea);
-
-        }
+        String shape=scanner.nextLine();
+       double area=0.0;
+       if (shape.equals("square")){
+           double side=Double.parseDouble(scanner.nextLine());
+           area=side*side;
+        }else if(shape.equals("rectangle")){
+           double sideA=Double.parseDouble(scanner.nextLine());
+           double sideB=Double.parseDouble(scanner.nextLine());
+           area=sideA*sideB;
+       } else if (shape.equals("circle")) {
+           double radius = Double.parseDouble(scanner.nextLine());
+           area = Math.PI * (radius * radius);
+       } else if (shape.equals("triangle")) {
+           double triangleA=Double.parseDouble(scanner.nextLine());
+           double triangleB=Double.parseDouble(scanner.nextLine());
+           area=(triangleA*triangleB)/2;
+       }
+        System.out.println(area);
     }
 }
